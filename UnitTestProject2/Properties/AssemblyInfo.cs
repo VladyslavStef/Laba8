d5fcbb7398a6fe38a8340256ee208ac0832879b6
юра hodan <yriy.khod@gmail.com>
2023-11-20 20:02:32 +0200

    Add project files.

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11c1ef31-9950-4b9f-9277-1d4189b077ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+
+
+class GitHub
+{
+
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+    public GitHub(ref IWebDriver driver)
+    {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    }
+    public void GoTo(string path)
+    {
+        driver.Navigate().GoToUrl(path);
+    }
+    public void Click(string path)
+    {
+        wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(path))).Click();
+
+    }
+    public bool IsPageLoaded()
+    {
+        return driver.Url.Contains("https://academy.ultimateqa.com/java-sdet");
+    }
+
+
+}
+
+class Class_for_change
+{
+
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+    public Class_for_change(ref IWebDriver driver)
+    {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    }
+    public void SendKeys(string id, string key)
+    {
+        var Send = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id(id)));
+        Send.SendKeys(key);
+        Send.SendKeys(Keys.Enter);
+    }
+    public void Click(string path)
+    {
+        var Cl = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(path)));
+
+        Cl.Click();
+    }
+    public bool IsPageLoaded()
+    {
+        return driver.Url.Contains("https://academy.ultimateqa.com/java-sdet");
+    }
+
+}
+
+
+
+
+[TestFixture]
+public class Tests_Class
+{
+
+    IWebDriver driver;
+    Class_for_change Change;
+    GitHub hub;
+    [SetUp]
+    public void SetUp()
+    {
+
+        new DriverManager().SetUpDriver(new EdgeConfig());
+
+        driver = new EdgeDriver();
+        driver.Manage().Window.Maximize();
+        hub = new GitHub(ref driver);
+        hub.GoTo("https://github.com");
+        Change = new Class_for_change(ref driver);
+    }
+
+
+    [Test]
+    public void TestMethod()
+    {
+
+        hub.Click("(//a[@class = 'HeaderMenu-link HeaderMenu-link--sign-up flex-shrink-0 d-none d-lg-inline-block no-underline border color-border-default rounded px-2 py-1'])[1]");
+
+        Change.SendKeys("email", "VladHJd@gmail.com");
+        Change.Click("(//button[@class = 'js-continue-button signup-continue-button form-control px-3 width-full width-sm-auto mt-4 mt-sm-0'])[1]");
+        Change.SendKeys("password", "12345678akdj");
+        Change.Click("(//button[@class = 'js-continue-button signup-continue-button form-control px-3 width-full width-sm-auto mt-4 mt-sm-0'])[2]");
+        Change.SendKeys("login", "vladstef32");
+        Change.Click("(//button[@class = 'js-continue-button signup-continue-button form-control px-3 width-full width-sm-auto mt-4 mt-sm-0'])[3]");
+        Change.Click("(//button[@class = 'js-continue-button signup-continue-button form-control px-3 width-full width-sm-auto mt-4 mt-sm-0'])[4]");
+
+    }
+}
